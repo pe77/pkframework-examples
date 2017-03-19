fa303753df557aa1fe0e11cbd0cb1b3c3ad27876
P <p321654987@outlook.com>
2017-03-19 20:36:48 -0300

    demo1 first release ok

--- com/gamebase/states/Main.ts
+++ com/gamebase/states/Main.ts
@@ -6,6 +6,11 @@ module GameBase
 
 	export class Main extends Pk.PkState {
 
+		cursors:Phaser.CursorKeys;
+		simon:Pk.PkElement;
+
+		speed:number = 3;
+
 		init(...args:any[])
 		{
 			console.log('Main init', args);
@@ -13,37 +18,74 @@ module GameBase
 
     	create()
     	{
-    		this.game.stage.backgroundColor = "#eeeeee";
-
     		console.log('Main create');
+			
+    		// create some elements
+    		this.simon 		= new Pk.PkElement(this.game);
+    		var stageBack 	= new Pk.PkElement(this.game);
+			var stageFront 	= new Pk.PkElement(this.game);
+			var pillars 	= new Pk.PkElement(this.game);
 
-    		// create two elements
-    		var simon = new Pk.PkElement(this.game);
-    		var square = new Pk.PkElement(this.game);
+			this.simon.enableBody = true;
 
-    		// event listener
-    		simon.event.add('onAutoWhip', (event, param1, param2, param3)=>{
-    			console.log('onAutoWhip reach! Params:', event, param1, param2, param3);
-    		});
-
-    		setTimeout(()=>{
-				// dispatch event
-    			simon.event.dispatch('onAutoWhip', 1, [1], 'one');
-    		}, 1000);
-    		
-    		// add/create sprite
-    		simon.add(this.game.add.sprite(0, 0, 'simon'));
-    		square.add(Pk.PkUtils.createSquare(this.game, 45, 45, "#ff00ff"));
-
-
-    		// add layers. yep!
+			// add layers
     		this.addLayer('stage-back');
     		this.addLayer('player');
     		this.addLayer('stage-front');
 
-    		this.addToLayer('player', simon);
-    		this.addToLayer('stage-back', square);
+    		// event listener
+    		this.simon.event.add('onAutoWhip', (event, param1, param2, param3)=>{
+    			console.log('onAutoWhip reach! Params:', event, param1, param2, param3);
+    		});
+
+			// dispatch event test 
+    		setTimeout(()=>{
+				// dispatch event
+    			this.simon.event.dispatch('onAutoWhip', 1, [1], 'one');
+    		}, 1000);
+    		
+    		// create sprites ... and add to element
+    		this.simon.add(this.game.add.sprite(0, 0, 'simon')); // player
+
+			// bg half size of world
+    		stageBack.add(Pk.PkUtils.createSquare(this.game, this.game.world.width, this.game.world.height/2, "#775577")); // stage bg (same size of world)
+			stageBack.y = this.game.world.centerY;
+
+			// create some pillars across the screen
+			var pillarsTotal = 5;
+			for (var i = 0; i < pillarsTotal; i++) {
+				var pillarSprite = Pk.PkUtils.createSquare(this.game, 40, this.game.world.height, "#000");
+				pillarSprite.x =  (this.game.world.width / pillarsTotal) * i;
+				pillars.add(pillarSprite);
+			}
+
+    		this.addToLayer('player', this.simon);
+    		this.addToLayer('stage-back', stageBack);
+			this.addToLayer('stage-front', pillars);
+
+			// put little simon on edge of screen and middle
+			this.simon.y = this.game.world.height - this.simon.height;
+			this.simon.x = this.game.world.centerX; // almost (not anchor /2 sprite size... but ok anyway)
+			
+			// add some interaction
+			this.cursors = this.game.input.keyboard.createCursorKeys();
     	}
+
+		update()
+		{
+			if (this.cursors.left.isDown)
+				this.simon.x -= this.speed; // moon walk
+			//
+			
+			if (this.cursors.right.isDown)
+				this.simon.x += this.speed;
+			//
+		}
+
+		render()
+		{
+			this.game.debug.text('User arrows [left/right] to walk', 32, 32);
+		}
     }
 
 }
--- dist/js/app.js
+++ dist/js/app.js
@@ -393,7 +393,9 @@ var GameBase;
     var Main = (function (_super) {
         __extends(Main, _super);
         function Main() {
-            return _super !== null && _super.apply(this, arguments) || this;
+            var _this = _super !== null && _super.apply(this, arguments) || this;
+            _this.speed = 3;
+            return _this;
         }
         Main.prototype.init = function () {
             var args = [];
@@ -403,28 +405,58 @@ var GameBase;
             console.log('Main init', args);
         };
         Main.prototype.create = function () {
-            this.game.stage.backgroundColor = "#eeeeee";
+            var _this = this;
             console.log('Main create');
-            // create two elements
-            var simon = new Pk.PkElement(this.game);
-            var square = new Pk.PkElement(this.game);
-            // event listener
-            simon.event.add('onAutoWhip', function (event, param1, param2, param3) {
-                console.log('onAutoWhip reach! Params:', event, param1, param2, param3);
-            });
-            setTimeout(function () {
-                // dispatch event
-                simon.event.dispatch('onAutoWhip', 1, [1], 'one');
-            }, 1000);
-            // add/create sprite
-            simon.add(this.game.add.sprite(0, 0, 'simon'));
-            square.add(Pk.PkUtils.createSquare(this.game, 45, 45, "#ff00ff"));
-            // add layers. yep!
+            // create some elements
+            this.simon = new Pk.PkElement(this.game);
+            var stageBack = new Pk.PkElement(this.game);
+            var stageFront = new Pk.PkElement(this.game);
+            var pillars = new Pk.PkElement(this.game);
+            this.simon.enableBody = true;
+            // add layers
             this.addLayer('stage-back');
             this.addLayer('player');
             this.addLayer('stage-front');
-            this.addToLayer('player', simon);
-            this.addToLayer('stage-back', square);
+            // event listener
+            this.simon.event.add('onAutoWhip', function (event, param1, param2, param3) {
+                console.log('onAutoWhip reach! Params:', event, param1, param2, param3);
+            });
+            // dispatch event test 
+            setTimeout(function () {
+                // dispatch event
+                _this.simon.event.dispatch('onAutoWhip', 1, [1], 'one');
+            }, 1000);
+            // create sprites ... and add to element
+            this.simon.add(this.game.add.sprite(0, 0, 'simon')); // player
+            // bg half size of world
+            stageBack.add(Pk.PkUtils.createSquare(this.game, this.game.world.width, this.game.world.height / 2, "#775577")); // stage bg (same size of world)
+            stageBack.y = this.game.world.centerY;
+            // create some pillars across the screen
+            var pillarsTotal = 5;
+            for (var i = 0; i < pillarsTotal; i++) {
+                var pillarSprite = Pk.PkUtils.createSquare(this.game, 40, this.game.world.height, "#000");
+                pillarSprite.x = (this.game.world.width / pillarsTotal) * i;
+                pillars.add(pillarSprite);
+            }
+            this.addToLayer('player', this.simon);
+            this.addToLayer('stage-back', stageBack);
+            this.addToLayer('stage-front', pillars);
+            // put little simon on edge of screen and middle
+            this.simon.y = this.game.world.height - this.simon.height;
+            this.simon.x = this.game.world.centerX; // almost (not anchor /2 sprite size... but ok anyway)
+            // add some interaction
+            this.cursors = this.game.input.keyboard.createCursorKeys();
+        };
+        Main.prototype.update = function () {
+            if (this.cursors.left.isDown)
+                this.simon.x -= this.speed; // moon walk
+            //
+            if (this.cursors.right.isDown)
+                this.simon.x += this.speed;
+            //
+        };
+        Main.prototype.render = function () {
+            this.game.debug.text('User arrows [left/right] to walk', 32, 32);
         };
         return Main;
     }(Pk.PkState));
