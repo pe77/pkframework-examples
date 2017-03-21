aa5409352582e1da72452eeb3f2e64d52b93f33c
P <p321654987@outlook.com>
2017-03-21 14:16:43 -0300

    add basic use for pkframe [state transition + param] and game structure proposal

---
+++ basic-starter/com/gamebase/states/Main.ts
@@ -0,0 +1,42 @@
+/// <reference path='../../../../com/pk/state/PkState.ts' />
+/// <reference path='../../../../com/pk/utils/PkUtils.ts' />
+
+module GameBase
+{
+
+	export class Main extends Pk.PkState {
+
+		enterKey:Phaser.Key;
+
+		init(...args:any[])
+		{
+			super.init(args); // if whant override init, you need this line!
+			console.log('Main init', args);
+		}
+
+    	create()
+    	{
+    		console.log('Main create');
+			
+    		// change state bg
+            this.game.stage.backgroundColor = "#938da0";
+
+    		// get the keyboard key to come back to menu
+            this.enterKey = this.game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+    	}
+
+        update()
+        {
+            // when press the key...
+            if (this.enterKey.isDown)
+                this.transition.change('Menu', 1111, 'text', {a:true, b:[1, 2]});  // return with some foo/bar args
+            //
+        }
+
+		render()
+		{
+			this.game.debug.text('Press [ENTER] to back to Menu', 35, 35);
+		}
+    }
+
+}
---
+++ basic-starter/com/gamebase/states/Menu.ts
@@ -0,0 +1,47 @@
+/// <reference path='../../../../com/pk/state/PkState.ts' />
+/// <reference path='../../../../com/pk/state/PkTransition.ts' />
+
+module GameBase
+{
+
+	export class Menu extends Pk.PkState {
+
+        spaceBarKey:Phaser.Key;
+
+		
+        init(param1, param2, param3) // or | init(...args:any[]) |
+		{
+            super.init(); // if whant override init, you need this line!
+			console.log('Menu init');
+
+            console.log('params:', param1, param2, param3);
+		}
+
+    	create()
+    	{
+            super.create();
+    		console.log('Menu create');
+
+            // change state bg
+            this.game.stage.backgroundColor = "#89aca6";
+
+    		// get the keyboard
+            this.spaceBarKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    		
+    	}
+
+        update()
+        {
+            // when press the key...
+            if (this.spaceBarKey.isDown)
+                this.transition.change('Main');  // change to main state (somenthing like game first stage or options state)
+            //
+        }
+
+        render()
+        {
+            this.game.debug.text('Press [SPACEBAR] to enter', 35, 35);
+        }
+    }
+
+}
